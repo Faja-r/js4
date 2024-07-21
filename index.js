d531d7c12a5fe9e268da9afecd9e99face2a3c8e
Faja-r <143716722+Faja-r@users.noreply.github.com>
2024-07-22 00:13:56 +0500

    new

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const apikey = "9bf2b6f8d80987ac43b0e0b8c27f9fd2";
+const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const searchInput=document.querySelector(".search input")
+const searchbutton=document.querySelector(".search button")
 
-const Apikey="9bf2b6f8d80987ac43b0e0b8c27f9fd2";
-const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
-
-async function checkWeather(){
-    const response = await fetch(apiurl + '&appid=${Apikey}');
-    var data = await response.json();
+async function checkweather(api_url) {
+    const response = await fetch(api_url);
+    const data = await response.json();
     console.log(data);
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+ "%";
+    document.querySelector(".wind").innerHTML=data.wind.speed + "km/h";
 }
-checkWeather();
+searchbutton.addEventListener("click", ()=>{
+    checkweather(searchInput.value);
+})
+
+
